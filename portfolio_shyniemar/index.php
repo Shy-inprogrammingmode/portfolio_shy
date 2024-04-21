3d28fb18d28edbf425700298eb7f90aa873e0818
s_hassan <149887565+shyhassan@users.noreply.github.com>
2024-04-22 00:54:08 +0800

    Add files via upload

--- portfolio_shyniemar/index.php
+++ portfolio_shyniemar/index.php
@@ -24,11 +24,9 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
   <link href="assets2/vendor/aos/aos.css" rel="stylesheet">
-
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Permanent+Marker&family=Ultra&display=swap" rel="stylesheet">
-
 <body>
           <audio id="backgroundMusic" controls>
         Your browser does not support the audio tag.
@@ -104,10 +102,8 @@
 </header>
 
 <?php
-// Include the connection file
 include 'includes/conn.php';
 
-// Query to fetch introduction data
 $sql = "SELECT * FROM intro";
 $stmt = $Conn->prepare($sql);
 $stmt->execute();
@@ -124,6 +120,7 @@ $intro = $stmt->fetch(PDO::FETCH_ASSOC);
             <h2 data-aos="fade-right" >IM <ndsp> <?php echo isset($intro['name']) ? $intro['name'] : ''; ?></h2>
             <p data-aos="fade-up" ><?php echo isset($intro['description']) ? $intro['description'] : ''; ?></p>
             <a data-aos="fade-up"  class="get"  href=""><span >hire me in</span></a>
+          
           </div>
           
           <div class="left-d" data-aos="fade-left" >
@@ -145,7 +142,7 @@ $intro = $stmt->fetch(PDO::FETCH_ASSOC);
             <a class="get2" href=""><span><?php echo isset($intro['dialect']) ? $intro['dialect'] : ''; ?></span></a>
           </div>
           <div class="down-d" data-aos="fade-right">
-          <a class="get2" href=""><span><?php echo isset($intro['role']) ? $intro['role'] : ''; ?></span></a>
+          <a class="get3" href=""><span><?php echo isset($intro['role']) ? $intro['role'] : ''; ?></span></a>
           </div>
         </section>
         </aside>
@@ -153,7 +150,6 @@ $intro = $stmt->fetch(PDO::FETCH_ASSOC);
     </div>
   </div>
 </section>
-
 
 <script>
 const cuteCard = document.getElementById('cuteCard');
@@ -425,6 +421,9 @@ function moveCard(event) {
     margin-bottom: 1rem;
     margin-left: 15%;
 }
+.hobbies-skills{
+  margin-top: 60px;
+}
   </style>
 <!-- Hobbies Skills Section -->
 <div class="hobbies-skills" data-aos="fade-left">
@@ -450,7 +449,39 @@ function moveCard(event) {
             </div>
         </div>
     </div>
+    <div class="cv-1">
+    <?php
+    $sql = "SELECT cv_file FROM cv LIMIT 1";
+    $stmt = $Conn->query($sql);
+    
+    if ($stmt !== false && $stmt->rowCount() > 0) {
+        $cv_row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $cv_file_name = $cv_row['cv_file'];
+        echo '<a data-aos="fade-up" class="cv" href="./admin/' . $cv_file_name . '">DOWNLOAD MY CV</a>';
+    } else {
+        echo 'No CV available';
+    }
+    ?>
 </div>
+
+</div>
+<style>
+  .cv-1 .cv{
+    background: #544d43;
+  padding: 12px 20px; /* Padding */
+  margin: 0px auto !important;
+  font-family: "Libre Baskerville", serif;
+  font-weight: 400;
+  font-style: italic;
+  font-size: 15px;
+  border-radius: 10px;
+  color: #fff; 
+  text-decoration: none;
+  }
+  .cv-1.cv:hover{
+    background: #e18f23; 
+  }
+</style>
 
   </section>
   <!-- Carousel Section -->
@@ -521,14 +552,14 @@ function moveCard(event) {
      <div class="gallery-shy" data-aos="zoom-in-up">
          <?php
 
-         // Fetch gallery images from the database
+         // Fetch gallery images from the database sheehsh zuragit a
          $sql = "SELECT * FROM gallery";
          $stmt = $Conn->query($sql);
          $galleryImages = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
          // Looping yan 
          foreach ($galleryImages as $image) {
-             // proper path makes eveythinig easy
+             // proper path makes eveythinig easygit
              $imagePath = "admin/" . $image['gallery_image'];
              echo '<div class="item" style="background-image: url(\'' . $imagePath . '\');"></div>';
          }
@@ -580,19 +611,42 @@ function moveCard(event) {
       </form>
     </div>
   </div>
+  
+<?php
+include 'includes/conn.php';
+
+$sql = "SELECT * FROM intro";
+$stmt = $Conn->prepare($sql);
+$stmt->execute();
+$intro = $stmt->fetch(PDO::FETCH_ASSOC);
+?>
   <div class="green" data-aos="fade-in">
-    <img  class ="imgcontact"src="assets2/img/cute.png" alt="">
+  <?php if (!empty($intro['image'])): ?>
+              <img src="admin/<?php echo $intro['image']; ?>" alt="model" />
+          <?php endif; ?>   
   </div>
 </section>
   
 
+<?php
+include './includes/conn.php';
+
+$sql = "SELECT * FROM social_links";
+$stmt = $Conn->prepare($sql);
+$stmt->execute();
+$socialLinks = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <footer id="footer">
     <div class="social-buttons" data-aos="fade-in">
-      <a href="#" class="social-button github">
-        <svg class="cf-icon-svg" xmlns="http://www.w3.org/2000/svg" viewBox="-2.5 0 19 19"><path d="M9.464 17.178a4.506 4.506 0 0 1-2.013.317 4.29 4.29 0 0 1-2.007-.317.746.746 0 0 1-.277-.587c0-.22-.008-.798-.012-1.567-2.564.557-3.105-1.236-3.105-1.236a2.44 2.44 0 0 0-1.024-1.348c-.836-.572.063-.56.063-.56a1.937 1.937 0 0 1 1.412.95 1.962 1.962 0 0 0 2.682.765 1.971 1.971 0 0 1 .586-1.233c-2.046-.232-4.198-1.023-4.198-4.554a3.566 3.566 0 0 1 .948-2.474 3.313 3.313 0 0 1 .091-2.438s.773-.248 2.534.945a8.727 8.727 0 0 1 4.615 0c1.76-1.193 2.532-.945 2.532-.945a3.31 3.31 0 0 1 .092 2.438 3.562 3.562 0 0 1 .947 2.474c0 3.54-2.155 4.32-4.208 4.548a2.195 2.195 0 0 1 .625 1.706c0 1.232-.011 2.227-.011 2.529a.694.694 0 0 1-.272.587z"></path></svg>
-          </a>
-          <!-- <a href="#" class="social-button linkedin">
-            <svg viewBox="0 -2 44 44" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <?php foreach ($socialLinks as $socialLink) : ?>
+            <a href="<?= $socialLink['link'] ?>" class="social-button <?= $socialLink['platform'] ?>">
+                <?php if ($socialLink['platform'] === 'github') : ?>
+                    <svg class="cf-icon-svg" xmlns="http://www.w3.org/2000/svg" viewBox="-2.5 0 19 19">
+                        <path d="M9.464 17.178a4.506 4.506 0 0 1-2.013.317 4.29 4.29 0 0 1-2.007-.317.746.746 0 0 1-.277-.587c0-.22-.008-.798-.012-1.567-2.564.557-3.105-1.236-3.105-1.236a2.44 2.44 0 0 0-1.024-1.348c-.836-.572.063-.56.063-.56a1.937 1.937 0 0 1 1.412.95 1.962 1.962 0 0 0 2.682.765 1.971 1.971 0 0 1 .586-1.233c-2.046-.232-4.198-1.023-4.198-4.554a3.566 3.566 0 0 1 .948-2.474 3.313 3.313 0 0 1 .091-2.438s.773-.248 2.534.945a8.727 8.727 0 0 1 4.615 0c1.76-1.193 2.532-.945 2.532-.945a3.31 3.31 0 0 1 .092 2.438 3.562 3.562 0 0 1 .947 2.474c0 3.54-2.155 4.32-4.208 4.548a2.195 2.195 0 0 1 .625 1.706c0 1.232-.011 2.227-.011 2.529a.694.694 0 0 1-.272.587z"></path>
+                    </svg>
+                <?php elseif ($socialLink['platform'] === 'linkedin') : ?>
+                  <svg viewBox="0 -2 44 44" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
             <g id="Icons" stroke="none" stroke-width="1">
                 <g transform="translate(-702.000000, -265.000000)">
                     <path d="M746,305 L736.2754,305 L736.2754,290.9384 C736.2754,287.257796 734.754233,284.74515 731.409219,284.74515 C728.850659,284.74515 727.427799,286.440738 726.765522,288.074854 C726.517168,288.661395 726.555974,289.478453 726.555974,290.295511 L726.555974,305 L716.921919,305 C716.921919,305 717.046096,280.091247 716.921919,277.827047 L726.555974,277.827047 L726.555974,282.091631 C727.125118,280.226996 730.203669,277.565794 735.116416,277.565794 C741.21143,277.565794 746,281.474355 746,289.890824 L746,305 L746,305 Z M707.17921,274.428187 L707.117121,274.428187 C704.0127,274.428187 702,272.350964 702,269.717936 C702,267.033681 704.072201,265 707.238711,265 C710.402634,265 712.348071,267.028559 712.41016,269.710252 C712.41016,272.34328 710.402634,274.428187 707.17921,274.428187 L707.17921,274.428187 L707.17921,274.428187 Z M703.109831,277.827047 L711.685795,277.827047 L711.685795,305 L703.109831,305 L703.109831,277.827047 L703.109831,277.827047 Z" id="LinkedIn">
@@ -600,9 +654,8 @@ function moveCard(event) {
                 </g>
             </g>
         </svg>
-      </a> -->
-      <a href="#" class="social-button facebook">
-        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 310 310" xml:space="preserve">
+                <?php elseif ($socialLink['platform'] === 'facebook') : ?>
+                  <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 310 310" xml:space="preserve">
     <g id="XMLID_834_">
       <path id="XMLID_835_" d="M81.703,165.106h33.981V305c0,2.762,2.238,5,5,5h57.616c2.762,0,5-2.238,5-5V165.765h39.064
         c2.54,0,4.677-1.906,4.967-4.429l5.933-51.502c0.163-1.417-0.286-2.836-1.234-3.899c-0.949-1.064-2.307-1.673-3.732-1.673h-44.996
@@ -611,9 +664,8 @@ function moveCard(event) {
         c-2.762,0-5,2.238-5,5v50.844C76.703,162.867,78.941,165.106,81.703,165.106z"></path>
     </g>
     </svg>
-      </a>
-      <a href="#" class="social-button instagram">
-        <svg width="800px" height="800px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <?php elseif ($socialLink['platform'] === 'instagram') : ?>
+                  <svg width="800px" height="800px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
         <g id="Page-1" stroke="none" stroke-width="1">
             <g id="Dribbble-Light-Preview" transform="translate(-340.000000, -7439.000000)">
                 <g id="icons" transform="translate(56.000000, 160.000000)">
@@ -624,9 +676,27 @@ function moveCard(event) {
             </g>
         </g>
     </svg>
-      </a>
+
+
+    
+    <?php elseif ($socialLink['github'] === 'github') : ?>
+      <svg class="cf-icon-svg" xmlns="http://www.w3.org/2000/svg" viewBox="-2.5 0 19 19"><path d="M9.464 17.178a4.506 4.506 0 0 1-2.013.317 4.29 4.29 0 0 1-2.007-.317.746.746 0 0 1-.277-.587c0-.22-.008-.798-.012-1.567-2.564.557-3.105-1.236-3.105-1.236a2.44 2.44 0 0 0-1.024-1.348c-.836-.572.063-.56.063-.56a1.937 1.937 0 0 1 1.412.95 1.962 1.962 0 0 0 2.682.765 1.971 1.971 0 0 1 .586-1.233c-2.046-.232-4.198-1.023-4.198-4.554a3.566 3.566 0 0 1 .948-2.474 3.313 3.313 0 0 1 .091-2.438s.773-.248 2.534.945a8.727 8.727 0 0 1 4.615 0c1.76-1.193 2.532-.945 2.532-.945a3.31 3.31 0 0 1 .092 2.438 3.562 3.562 0 0 1 .947 2.474c0 3.54-2.155 4.32-4.208 4.548a2.195 2.195 0 0 1 .625 1.706c0 1.232-.011 2.227-.011 2.529a.694.694 0 0 1-.272.587z"></path></svg>
+          </a>
+          <a href="#" class="social-button linkedin">
+            <svg viewBox="0 -2 44 44" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <g id="Icons" stroke="none" stroke-width="1">
+                <g transform="translate(-702.000000, -265.000000)">
+                    <path d="M746,305 L736.2754,305 L736.2754,290.9384 C736.2754,287.257796 734.754233,284.74515 731.409219,284.74515 C728.850659,284.74515 727.427799,286.440738 726.765522,288.074854 C726.517168,288.661395 726.555974,289.478453 726.555974,290.295511 L726.555974,305 L716.921919,305 C716.921919,305 717.046096,280.091247 716.921919,277.827047 L726.555974,277.827047 L726.555974,282.091631 C727.125118,280.226996 730.203669,277.565794 735.116416,277.565794 C741.21143,277.565794 746,281.474355 746,289.890824 L746,305 L746,305 Z M707.17921,274.428187 L707.117121,274.428187 C704.0127,274.428187 702,272.350964 702,269.717936 C702,267.033681 704.072201,265 707.238711,265 C710.402634,265 712.348071,267.028559 712.41016,269.710252 C712.41016,272.34328 710.402634,274.428187 707.17921,274.428187 L707.17921,274.428187 L707.17921,274.428187 Z M703.109831,277.827047 L711.685795,277.827047 L711.685795,305 L703.109831,305 L703.109831,277.827047 L703.109831,277.827047 Z" id="LinkedIn">
+            </path>
+                </g>
+            </g>
+        </svg>   
+         <?php endif; ?>
+            </a>
+        <?php endforeach; ?>
     </div>
 </footer>
+
 <style>
 .shyme{
   background-color: #F6EEE1;
@@ -905,15 +975,13 @@ function positionExperienceCard() {
     // Calculate the left position to center the card horizontally
     var leftPosition = (viewportWidth - experienceCard.offsetWidth) / 2;
 
-    // You can adjust these values based on your requirements
+
     experienceCard.style.top = '215%'; // Adjust as needed
     experienceCard.style.left = '25.5%'; // Adjust left position here
 
-    // If you want to use the centered horizontal position, you can uncomment the line below
-    // experienceCard.style.left = leftPosition + 'px';
+
 }
 
-// Call the function initially and whenever the window is resized
 window.addEventListener('resize', positionExperienceCard);
 window.addEventListener('load', positionExperienceCard);
 
@@ -940,8 +1008,6 @@ window.addEventListener('load', positionExperienceCard);
 </script>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 </html>
-
-
 
 <style>
   .cutekut {
